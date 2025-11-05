870cb7dd3fd6a3df7a580a421c7c40a8fead3eff
arturito-7 <argome04@ucm.es>
2025-11-05 23:15:13 +0100

    Lista de reservas actualizada
    Lista de reservas se ve distinto si eres admin o empleado. Si eres admin tienes un filtro. Y en el nav ves distintos botones dependiendo de tu rol

--- recurso-bootstrap/routes/reservas.js
+++ recurso-bootstrap/routes/reservas.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-const { isAuth } = require('../middleware/auth');
+const { isAuth, isAdmin } = require('../middleware/auth');
+
+const store = require('../data/store');
 
 router.get('/', isAuth, (req, res) => {
     res.render('reservas', { title: 'Reserva' });
 });
 
 router.post('/', isAuth, (req, res) => {
-    const { reservas } = req.app.locals.store;
+    const { reservas } = store;
     const datosRecibidos = req.body;
 
-    // Aqui deberias anyadir validacion (LAB 8/9)
-    // y el id_usuario de la sesion (LAB 7)
+    datosRecibidos.id_usuario = req.session.usuario.id_usuario; 
+    datosRecibidos.usuarioCorreo = req.session.usuario.correo;
+    datosRecibidos.usuarioNombre = req.session.usuario.nombre;
 
     reservas.push(datosRecibidos);
 
@@ -21,20 +24,45 @@ router.post('/', isAuth, (req, res) => {
     console.log("--- Total de reservas en memoria ---");
     console.table(reservas);
 
-    res.redirect('/reserva/listareservas');
+    if (req.session.usuario.rol === 'admin') {
+        return res.redirect('/reserva/listareservas');
+    }else{
+        return res.redirect('/reserva/mis-reservas');
+    }
 });
 
-router.get('/listareservas', isAuth, (req, res) => {
-    const { reservas } = req.app.locals.store;
+router.get('/listareservas', isAdmin, (req, res) => {
+    const { reservas, usuarios } = store;
+    const idFiltrado = req.query.id;
+
+    let reservasAMostrar = reservas;
+
+    if (idFiltrado) {
+        const idNumero = parseInt(idFiltrado, 10);
+        reservasAMostrar = reservas.filter(r => r.id_usuario === idNumero);
+    }
+
     res.render('listareservas', {
         title: 'Lista de Reservas',
-        listaDeReservas: reservas
+        listaDeReservas: reservasAMostrar,
+        todosLosUsuarios: usuarios,
+        idSeleccionado: parseInt(idFiltrado, 10) || 0
+    });
+});
+
+router.get('/mis-reservas', isAuth, (req, res) => {
+    const { reservas } = store;
+    const usuarioActual = req.session.usuario;
+    const reservasDelUsuario = reservas.filter(reserva => reserva.id_usuario === usuarioActual.id_usuario);
+    res.render('listareservas', {
+        title: 'Mis Reservas',
+        listaDeReservas: reservasDelUsuario,
+        todosLosUsuarios: [],
+        idSeleccionado: 0
     });
 });
 
 // Aqui iran rutas futuras como:
-// GET /mis-reservas (para usuarios logueados)
 // POST /api/reservas (para AJAX en LAB 9)
-
 
 module.exports = router;
