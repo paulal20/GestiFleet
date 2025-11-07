2fdff1b5d1b824c7a2774aac96c57f5edb84c9f0
arturito-7 <argome04@ucm.es>
2025-11-07 13:16:44 +0100

    Metidos los vehiculos al script

--- recurso-bootstrap/script.sql
+++ recurso-bootstrap/script.sql
@@ -62,3 +62,22 @@ INSERT INTO concesionarios (id_concesionario, nombre, ciudad, direccion, telefon
 INSERT INTO usuarios (id_usuario, nombre, correo, contraseña, rol, id_concesionario) VALUES 
 (1, 'Administrador', 'admin@gestifleet.com', 'Admin^12', 'admin', 1),
 (2, 'Empleado Ejemplo', 'empleado@gestifleet.com', 'Empleado^1', 'empleado', 2);
+
+INSERT INTO vehiculos (id_vehiculo, matricula, marca, modelo, año_matriculacion, numero_plazas, autonomia_km, color, imagen, estado, id_concesionario) VALUES
+(1,  'MAD0001A', 'BYD',        'Seal 1',               2023, 5, 520, 'Gris', '/img/vehiculos/byd_seal1.png', 'disponible',   1),
+(2,  'MAD0002A', 'BYD',        'Seal 2',               2023, 5, 500, 'Negro', '/img/vehiculos/byd_seal2.png', 'disponible',   1),
+(3,  'MAD0003A', 'BYD',        'Seal 3',               2024, 5, 540, 'Blanco', '/img/vehiculos/byd_seal3.png', 'mantenimiento',2),
+(4,  'MAD0004A', 'Tesla',      'Model S',              2023, 5, 652, 'Negro', '/img/vehiculos/tesla1.png', 'reservado',    1),
+(5,  'MAD0005A', 'Tesla',      'Model 3',              2024, 5, 560, 'Rojo', '/img/vehiculos/tesla2.png', 'disponible',   1),
+(6,  'MAD0006A', 'Tesla',      'Model X',              2023, 7, 600, 'Gris', '/img/vehiculos/tesla3.png', 'disponible',   2),
+(7,  'MAD0007A', 'Tesla',      'Model Y',              2024, 5, 540, 'Negro', '/img/vehiculos/tesla4.png', 'disponible',   1),
+(8,  'MAD0008A', 'Tesla',      'Model 3 Urban',        2023, 5, 430, 'Azul', '/img/vehiculos/tesla5.png', 'reservado',    2),
+(9,  'MAD0009A', 'Tesla',      'Model X Family',       2024, 7, 580, 'Blanco', '/img/vehiculos/tesla6.png', 'disponible',   1),
+(10, 'MAD0010A', 'Volvo',      'XC90',                 2023, 7, 550, 'Gris', '/img/vehiculos/volvo1.png', 'disponible',   2),
+(11, 'MAD0011A', 'Volvo',      'XC90 Advanced',        2024, 7, 560, 'Blanco', '/img/vehiculos/volvo2.png', 'mantenimiento',1),
+(12, 'MAD0012A', 'Volvo',      'XC90 Premium',         2023, 7, 570, 'Amarillo', '/img/vehiculos/volvo3.png', 'disponible',   2),
+(13, 'MAD0013A', 'Volkswagen', 'T-Cross',              2023, 5, 650, 'Azul', '/img/vehiculos/VW1.png', 'disponible',   1),
+(14, 'MAD0014A', 'Volkswagen', 'T-Roc',                2024, 5, 630, 'Rojo', '/img/vehiculos/VW2.png', 'reservado',    2),
+(15, 'MAD0015A', 'Volkswagen', 'Tiguan',               2023, 5, 620, 'Azul', '/img/vehiculos/VW3.png', 'disponible',   1),
+(16, 'MAD0016A', 'Volkswagen', 'Tiguan Sport',         2024, 5, 610, 'Rojo', '/img/vehiculos/VW4.png', 'disponible',   2),
+(17, 'MAD0017A', 'Volkswagen', 'Tiguan Family',        2023, 7, 600, 'Azul', '/img/vehiculos/VW5.png', 'disponible',   1);
